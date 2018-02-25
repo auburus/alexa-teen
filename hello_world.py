dfb465992087d584b18b5fbad5df32fa22a27c35
Alex <avlasse81@gmail.com>
2018-02-25 10:44:21 -0700

    Modified default response

--- hello_world.py
+++ hello_world.py
@@ -68,7 +68,7 @@ def on_intent(request, session):
     elif intent.name == 'hello':
         return intent.hello()
     else:
-        return response({}, response_ssml_text(ssml_rate_tag(ssml_pitch_tag("I am groot!"), "high"), "fast"))
+        return response({}, response_ssml_text(ssml_rate_tag("I am groot!"), "fast"))
 
 # We can't send a response back on a session end
 def on_session_ended(request, session):
