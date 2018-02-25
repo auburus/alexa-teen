92df07eefefd5feda2ef1103fd28bf1984257292
Alex <avlasse81@gmail.com>
2018-02-25 11:30:18 -0700

    Modified weather intent responses with different ssml

--- hello_world.py
+++ hello_world.py
@@ -156,8 +156,8 @@ messages = {
         ssml_rate_tag('What the ' +ssml_explitive_tag('fuck ') +'do you want now?', 'fast'),
     ],
     INTENT_WEATHER: [
-        ssml_whisper_tag('As if you really need to fucking know that'),
-        ssml_emphasis_tag('Do I look like I know fucking everything?'),
+        ssml_rate_tag('As if you ' +ssml_rate_tag('really', 'slow') +' need to know that', 'fast'),
+       'Do I look like I '  +ssml_emphasis_tag('really know') + '  every fucking thing?'),
         ssml_rate_tag(ssml_pitch_tag('The night is fucking dark and full of terrors','low'), 'slow'),
     ]
 }
